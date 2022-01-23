f2042518b1c3ef83283d3469ea2b9652195c3c0d
Bumboobee <94147847+Bumboobee@users.noreply.github.com>
2022-01-22 21:31:28 -0300

    Final Website

---
+++ script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector(".navbarr__menu");
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+ScrollReveal({ 
+    reset: true, 
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+ScrollReveal().reveal('.about', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.content', { delay: 400, origin: 'left'});
+ScrollReveal().reveal('.main__img--container', { delay: 700, origin: 'bottom'});
+ScrollReveal().reveal('.work, .time, .right-side', { delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.time', { delay: 700, origin: 'bottom'});
+ScrollReveal().reveal('.title', { delay: 400, origin: 'right'});
